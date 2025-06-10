36857cf5ff7d1ed643439d7ed655e360950e1f24
Bonmyeong Koo - vn56eqd <Bonmyeong.Koo@walmart.com>
2025-06-10 10:31:57 -0400

    Initial Commit

---
+++ MachineLearningSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MachineLearningSwift
+//
+//  Created by Bonmyeong Koo - Vendor on 6/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MachineLearningSwift/MachineLearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MachineLearningSwiftApp.swift
+//  MachineLearningSwift
+//
+//  Created by Bonmyeong Koo - Vendor on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MachineLearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
